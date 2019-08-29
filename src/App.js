8ea4e2ec316224cf57f5ab15415c283e99080b7b
Alc499 <Ali.vitchenko@gmail.com>
2019-08-29 11:25:06 +0300

    todos loadig was implemented

--- src/App.js
+++ src/App.js
@@ -1,52 +1,61 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import {
+  loadTodos,
+  clearTodos,
+  getTodosFromState,
+  getCurrentUser,
+  getIsLoading,
+  getHasError,
+} from './store';
 
 import CurrentUser from './CurrentUser';
 import TodoList from './TodoList';
-import { getTodos } from './api';
 
-getTodos()
-  .then(todos => {
-    console.log(todos);
-  });
+// getTodos()
+//   .then(todos => {
+//     console.log(todos);
+//   });
 
-const todos = [
-  {
-    userId: 1,
-    id: 1,
-    title: 'delectus aut autem',
-    completed: true,
-  },
-  {
-    userId: 2,
-    id: 2,
-    title: 'quis ut nam facilis et officia qui',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 3,
-    title: 'fugiat veniam minus',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 4,
-    title: 'et porro tempora',
-    completed: true,
-  },
-  {
-    userId: 4,
-    id: 5,
-    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 6,
-    title: 'qui ullam ratione quibusdam voluptatem quia omnis',
-    completed: false,
-  },
-];
+// const todos = [
+//   {
+//     userId: 1,
+//     id: 1,
+//     title: 'delectus aut autem',
+//     completed: true,
+//   },
+//   {
+//     userId: 2,
+//     id: 2,
+//     title: 'quis ut nam facilis et officia qui',
+//     completed: false,
+//   },
+//   {
+//     userId: 1,
+//     id: 3,
+//     title: 'fugiat veniam minus',
+//     completed: false,
+//   },
+//   {
+//     userId: 1,
+//     id: 4,
+//     title: 'et porro tempora',
+//     completed: true,
+//   },
+//   {
+//     userId: 4,
+//     id: 5,
+//     title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+//     completed: false,
+//   },
+//   {
+//     userId: 1,
+//     id: 6,
+//     title: 'qui ullam ratione quibusdam voluptatem quia omnis',
+//     completed: false,
+//   },
+// ];
 const currentUser = {
   id: 1,
   name: 'Leanne Graham',
@@ -71,73 +80,142 @@ const currentUser = {
   },
 };
 
-const App = () => (
-  <main className="App">
-    <section>
-      <p className="info">
-        Todos are not loaded yet
-        <button type="button">Load</button>
-      </p>
+class App extends React.Component {
+  render() {
+    const {
+      todos,
+      loadTodos,
+      clearTodos,
+      isLoading,
+      hasError,
+    } = this.props;
 
-      <p className="info">User is not selected</p>
-    </section>
+    if (isLoading) {
+      return (
+        <main className="App">
+          <section>
+            <p className="info">Loading...</p>
+            <p className="info">Loading...</p>
+          </section>
 
-    <section>
-      <p className="info">Loading...</p>
-      <p className="info">Loading...</p>
-    </section>
+          <section>
+            <div className="content">
+              <div className="loader" />
+            </div>
+            <div className="content">
+              <div className="loader" />
+            </div>
+          </section>
+        </main>
+      )
+    }
 
-    <section>
-      <p className="info">
-        Failed loading todos
-        <button type="button">Reload</button>
-      </p>
+    if (hasError) {
+      return (
+        <main className="App">
+          <section>
+            <p className="info">
+              Failed loading todos
+              <button type="button">Reload</button>
+            </p>
 
-      <p className="info">
-        Failed loading user
-        <button type="button">Reload</button>
-      </p>
-    </section>
+            <p className="info">
+              Failed loading user
+              <button type="button">Reload</button>
+            </p>
+          </section>
 
-    <section>
-      <p className="info">
-        6 todos are loaded
-        <button type="button">Clear</button>
-      </p>
+          <section>
+            <div className="content">
+              <p>-</p>
+            </div>
 
-      <p className="info">
-        User #1 is loaded
-        <button type="button">Clear</button>
-      </p>
-    </section>
+            <div className="content">
+              <p>-</p>
+            </div>
+          </section>
+        </main>
+      )
+    }
 
-    <section>
-      <p className="info">
-        1 todo is loaded
-        <button type="button">Clear</button>
-      </p>
+    if (todos.length < 1) {
+      return (
+        <main className="App">
+          <section>
+            <p className="info">
+              Todos are not loaded yet
+              <button
+                type="button"
+                onClick={loadTodos}
+              >Load</button>
+            </p>
 
-      <p className="info">User #999 does not exist</p>
-    </section>
+            <p className="info">User is not selected</p>
+          </section>
 
-    <section>
-      <div className="content">
-        <div className="loader" />
-      </div>
-      <div className="content">
-        <CurrentUser user={currentUser} />
-      </div>
-    </section>
+          <section>
+            <div className="content">
+              <p>-</p>
+            </div>
 
-    <section>
-      <div className="content">
-        <TodoList todos={todos} />
-      </div>
-      <div className="content">
-        <div className="loader" />
-      </div>
-    </section>
-  </main>
-);
+            <div className="content">
+              <p>-</p>
+            </div>
+          </section>
+        </main>
+      )
+    }
 
-export default App;
+    return (
+      <main className="App">
+        <section>
+          <p className="info">
+            6 todos are loaded
+            <button
+              type="button"
+              onClick={clearTodos}
+            >
+              Clear
+            </button>
+          </p>
+
+          <p className="info">
+            User #1 is loaded
+            <button type="button">Clear</button>
+          </p>
+        </section>
+
+        <section>
+          <p className="info">
+            1 todo is loaded
+            <button type="button">Clear</button>
+          </p>
+
+          <p className="info">User #999 does not exist</p>
+        </section>
+
+        <section>
+          <div className="content">
+            <TodoList todos={todos} />
+          </div>
+          <div className="content">
+            <CurrentUser user={currentUser} />
+          </div>
+        </section>
+      </main>
+    )
+  }
+};
+
+const getData = state => ({
+  todos: getTodosFromState(state),
+  isLoading: getIsLoading(state),
+  hasError: getHasError(state),
+});
+
+const getMethods = dispatch => ({
+  loadTodos: () => dispatch(loadTodos()),
+  clearTodos: () => dispatch(clearTodos()),
+});
+
+export default connect(getData, getMethods)(App);
--- src/store.js
+++ src/store.js
@@ -1,9 +1,81 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk  from 'redux-thunk';
 
-const initialState = {};
+
+import { getTodos, getUser } from './api';
+
+const START_LOADING = 'startLoading';
+const SET_TODOS = 'setTodos';
+const SET_USER = 'setUser';
+const HANDLE_ERROR = 'handleError';
+const CLEAR_TODOS = 'clearTodos';
+
+export const startLoading = () => ({ type: START_LOADING });
+export const handleError = () => ({ type: HANDLE_ERROR });
+
+export const setTodos = todos => ({ type: SET_TODOS, todos });
+export const setUser = user => ({ type: SET_USER, user });
+export const clearTodos = () => ({type: CLEAR_TODOS });
+
+export const loadTodos = () => {
+  return (dispatch) => {
+    dispatch(startLoading());
+
+    return getTodos()
+      .then(todos => dispatch(setTodos(todos)))
+      .catch(() => dispatch(handleError()));
+  };
+}
+
+export const getTodosFromState = state => state.todos;
+export const getCurrentUser = state => state.currentUser;
+export const getIsLoading = state => state.isLoading;
+export const getHasError = state => state.hasError;
+
+const initialState = {
+  todos: [],
+  currentUser: {},
+  isLoading: false,
+  hasError: false,
+};
 
 const reducer = (state, action) => {
+
   switch (action.type) {
+    case START_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        hasError: false,
+      };
+
+    case SET_TODOS:
+      return {
+        ...state,
+        todos: action.todos,
+        isLoading: false
+      }
+
+    case SET_USER:
+        return {
+          ...state,
+          currentUser: action.user,
+          isLoading: false
+        }
+
+    case HANDLE_ERROR:
+        return {
+          ...state,
+          hasError: true,
+          isLoading: false
+        }
+
+    case CLEAR_TODOS:
+      return {
+        ...state,
+        todos: [],
+      }
+
     default:
       return state;
   }
@@ -11,7 +83,8 @@ const reducer = (state, action) => {
 
 const store = createStore(
   reducer,
-  initialState
+  initialState,
+  applyMiddleware(thunk),
 );
 
 export default store;
